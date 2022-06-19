e14eeb5dc56df8082a2adc79d070a0e2c97ec581
Andres Saenz <asaenzcordero@gmail.com>
2022-06-18 22:29:31 -0500

    cuenta corriente entity

--- domain/src/main/java/co/com/banco/entity/CuentaCorriente.java
+++ domain/src/main/java/co/com/banco/entity/CuentaCorriente.java
@@ -1,4 +1,33 @@
 package co.com.banco.entity;
 
-public class CuentaCorriente {
+import co.com.banco.value.CantChequesDisponibles;
+import co.com.banco.value.CuentaCorrienteID;
+import co.com.banco.value.NumeroCuenta;
+import co.com.banco.value.SobreGiro;
+import co.com.sofka.domain.generic.Entity;
+
+public class CuentaCorriente extends Entity<CuentaCorrienteID> {
+    protected NumeroCuenta numeroCuenta;
+    protected SobreGiro sobreGiro;
+    protected CantChequesDisponibles cantChequesDisponibles;
+
+    public CuentaCorriente(CuentaCorrienteID cuentaCorrienteID, NumeroCuenta numeroCuenta, SobreGiro sobreGiro,
+                           CantChequesDisponibles cantChequesDisponibles) {
+        super(cuentaCorrienteID);
+        this.numeroCuenta = numeroCuenta;
+        this.sobreGiro = sobreGiro;
+        this.cantChequesDisponibles = cantChequesDisponibles;
+    }
+
+    public NumeroCuenta getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public SobreGiro getSobreGiro() {
+        return sobreGiro;
+    }
+
+    public CantChequesDisponibles getCantChequesDisponibles() {
+        return cantChequesDisponibles;
+    }
 }
